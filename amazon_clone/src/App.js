faa90ab4f5a9585949c2bdbf598233c1c705af57
kalpesh1213 <kalpeshsapkale12@gmail.com>
2021-05-25 17:20:24 +0530

    first commit

---
+++ amazon_clone/src/App.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="grid_container">
+      <header>
+        <a href ="/">React Shopping Cart</a>
+      </header>
+      <main> Product List  </main>
+      <footer> All rights are reserved. </footer>
+    </div>
+  );
+}
+
+export default App;
